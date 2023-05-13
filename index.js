ba8167dfec93df99cf598b55b6fcc5cba629d020
CodePathfinder <124159621+CaptSV@users.noreply.github.com>
2023-05-13 22:33:58 +0800

    Initial Upload
    Initial files

---
+++ index.js
@@ -0,0 +1,10 @@
+// selecting navbar link elements
+const menuLink = document.getElementById("menuLink");
+const cateringLink = document.getElementById("cateringLink");
+const careersLink = document.getElementById("careersLink");
+const blogLink = document.getElementById("blogLink");
+
+// add eventListener to navbar links
+menuLink.addEventListener("click", loadMenuPage);
+
+const loadMenuPage = (e) => {};
